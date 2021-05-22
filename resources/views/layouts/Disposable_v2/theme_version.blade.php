a08f3d33c007d0a7b32b38fa0a6698651ce2438a
B.Fatih KOZ <fatih.koz@gmail.com>
2021-05-22 18:55:35 +0300

    Update Theme Version

--- resources/views/layouts/Disposable_v2/theme_version.blade.php
+++ resources/views/layouts/Disposable_v2/theme_version.blade.php
@@ -1,1 +1,1 @@
-<span><a href="https://github.com/FatihKoz/disposable-themes" target="_blank" title="Version: v2.0.1 (11MAY21)">Disposable Theme</a></span>
+<span><a href="https://github.com/FatihKoz/disposable-themes" target="_blank" title="Version: v2.0.2 (22MAY21)">Disposable Theme</a></span>
